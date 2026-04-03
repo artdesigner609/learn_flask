ade8befd6f3370c9b4eced0e4109d0052df8085c
artdesigner609 <artdesigner609@gmail.com>
2026-04-03 10:29:20 -0400

    learn context sending to template and  linking endpoint to others

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
 #add default route
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    eng_desc = {
+        'name' : 'John Doe',
+        'age' : 30,
+        'city' : 'New York',
+        'salary' : 50000,
+        'education' : 'Computer Science'
+    }
+    return render_template('index.html', context=eng_desc)
 
+#add aboutme route
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
 
 
 if __name__ == "__main__":
